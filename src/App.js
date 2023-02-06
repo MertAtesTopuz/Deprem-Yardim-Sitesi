ff4a8bf2e94fdeb7c1d8ff382ccc4b14cb6473f4
Yiğit Doğan <39031288+ygtdgn@users.noreply.github.com>
2023-02-06 17:43:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [deprem, setDeprem] = useState("");
+
+  useEffect(() => {
+    fetch('https://api.orhanaydogdu.com.tr/deprem/live.php?limit=100')
+      .then(response => response.json())
+      .then(data => {
+        setDeprem(data);
+      });
+  }, []);
   return (
-    <>test</>
+    
   );
+
 }
 
 export default App;
