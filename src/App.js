15caff7e5061e284488422b19153e314c9d22b4f
Yiğit Doğan <39031288+ygtdgn@users.noreply.github.com>
2023-02-08 14:08:57 +0300

    Toplanma alanı sayfası eklendi

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import { Route, Routes, Link, Navlink } from 'react-router-dom';
 import './App.scss';
 import Deprem from './components/Deprem';
 import Yardim from './components/Yardim';
+import Toplanma from './components/Toplanma';
 
 
 function App() {
 
  
   return (
-    <>
-    <nav >
+    <div className='mainContainer'>
+    <nav>
       <Link className='link' to="/">Deprem Raporu</Link>
-      <Link className='link' to="yardim-kuruluslari">Yardım Kuruluşları</Link>
+      <Link className='link' to="toplanma-alanlari">Toplanma Alanları</Link>
+      <Link className='link' to="/yardim-kanallari">Yardım Kurumları</Link>
     </nav>
     <Routes>
       <Route path='/' element={<Deprem />} />
-      <Route path='/' element={<Yardim />} />
+      <Route path='/toplanma-alanlari' element={<Toplanma />} />
+      <Route path='/yardim-kanallari' element={<Yardim/>}/>
     </Routes>
-  </>
+  </div>
   );
 }
 
---
+++ src/components/Talep.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+function Talep() {
+
+    const [talepler, setTalepler] = useState([]);
+    
+
+
+    console.log(talepler)
+    return ( 
+        <div>
+            <h1>Talep Sayfası</h1>
+            
+        </div>
+     );
+}
+
+export default Talep;
